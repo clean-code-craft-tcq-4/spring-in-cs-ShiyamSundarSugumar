0bbe611f309f53446ac9268d8e9b67645be26961
ShiyamSundarSugumar <105203939+ShiyamSundarSugumar@users.noreply.github.com>
2022-05-09 19:29:41 +0530

    Update Statistics.cs

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -3,11 +3,40 @@ using System.Collections.Generic;
 
 namespace Statistics.Test
 {
-    public class StatsComputer
-    {
-        public Stats CalculateStatistics(List<float> numbers) {
+double sum = 0.0,average=0.0, min=0.0, max=0.0;
+            public object CalculateStatistics(List<float> numbers)
+            {
             //Implement statistics here
-            Console.WriteLines("shiyam");
-        }
-    }
+
+               for (int i=0; i<numbers.Count; i++)
+                {
+                    if(i==0)
+                    {
+                        min = max = numbers[i];
+
+                    }
+                    if (max < numbers[i])
+                    {
+                        max = numbers[i];
+                    }
+
+                    if (min > numbers[i])
+                    {
+                        min = numbers[i];
+                    }
+
+                    sum += numbers[i];
+
+                }
+
+
+                average = sum / numbers.Count;
+
+            
+                return numbers;
+                
+            }
+    
 }
+                
+          
