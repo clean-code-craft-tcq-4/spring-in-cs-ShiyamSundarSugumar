462bb176ac37297ece527d9ad4dc4a0ca2e80f8c
ShiyamSundarSugumar <105203939+ShiyamSundarSugumar@users.noreply.github.com>
2022-05-09 19:42:09 +0530

    Update Statistics.cs

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -3,37 +3,38 @@ using System.Collections.Generic;
 
 namespace Statistics.Test
 {
-double sum = 0.0,average=0.0, min=0.0, max=0.0;
-            public object CalculateStatistics(List<float> numbers)
+            public class StatsComputer
             {
-            //Implement statistics here
+                        double sum = 0.0,average=0.0, min=0.0, max=0.0;
+                        public object CalculateStatistics(List<float> numbers)
+                        {
+                        //Implement statistics here
+                           
+                           for (int i=0; i<numbers.Count; i++)
+                            {
+                                if(i==0)
+                                {
+                                    min = max = numbers[i];
 
-               for (int i=0; i<numbers.Count; i++)
-                {
-                    if(i==0)
-                    {
-                        min = max = numbers[i];
+                                }
+                                if (max < numbers[i])
+                                {
+                                    max = numbers[i];
+                                }
 
-                    }
-                    if (max < numbers[i])
-                    {
-                        max = numbers[i];
-                    }
+                                if (min > numbers[i])
+                                {
+                                    min = numbers[i];
+                                }
 
-                    if (min > numbers[i])
-                    {
-                        min = numbers[i];
-                    }
+                                sum += numbers[i];
 
-                    sum += numbers[i];
-
-                }
+                            }
 
 
-                average = sum / numbers.Count;
-
-            
-                return numbers;
+                            average = sum / numbers.Count;
+                            return numbers;
+                        }
                 
             }
     
