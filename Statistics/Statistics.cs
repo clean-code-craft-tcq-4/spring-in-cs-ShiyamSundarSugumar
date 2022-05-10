31b3e3922f1f71d4d8ce8328c345673e23951962
ShiyamSundarSugumar <105203939+ShiyamSundarSugumar@users.noreply.github.com>
2022-05-10 13:17:27 +0530

    Update Statistics.cs

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -6,34 +6,44 @@ namespace Statistics.Test
             public class StatsComputer
             {
                         public double sum = 0.0,average=0.0, min=0.0, max=0.0;
-                        public object CalculateStatistics(List<float> numbers)
+                        public object CalculateStatistics(List<double> numbers)
                         {
                         //Implement statistics here
                            
-                           for (int i=0; i<numbers.Count; i++)
-                            {
-                                if(i==0)
-                                {
-                                    min = max = numbers[i];
+                            // If Empty list arrived
+                           if (numbers == null )
+                           {
+                                       sum = average = min = max = Double.Nan
+                           }
+                           else
+                           {
+                                       
+                            
+                                       for (int i=0; i<numbers.Count; i++)
+                                        {
+                                            if(i==0)
+                                            {
+                                                min = max = numbers[i];
 
-                                }
-                                if (max < numbers[i])
-                                {
-                                    max = numbers[i];
-                                }
+                                            }
+                                            if (max < numbers[i])
+                                            {
+                                                max = numbers[i];
+                                            }
 
-                                if (min > numbers[i])
-                                {
-                                    min = numbers[i];
-                                }
+                                            if (min > numbers[i])
+                                            {
+                                                min = numbers[i];
+                                            }
 
-                                sum += numbers[i];
+                                            sum += numbers[i];
 
-                            }
+                                        }
 
 
                             average = sum / numbers.Count;
                             return numbers;
+                           }
                         }
                 
             }
